f63f4c14fcff2e6c2afd993c7d0d9e4812323155
EvyaSup <116467125+EvyaSup@users.noreply.github.com>
2022-10-23 02:10:07 +0300

    Add files via upload

---
+++ Constants.php
@@ -0,0 +1,10 @@
+<?php
+
+class Constants
+{
+    public static $botApiKey = "5445243658:AAHiyecca0pFkkrOKKaI9pQ9FHuWSoDIHwI";
+    public static $botUsername = "GallerytestitBot";
+    public static $hookUrl = 'https://github.com/EvyaSup/TelegramGallery/index.php'; // your index.php url
+
+
+}
