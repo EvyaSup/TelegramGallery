97dcdfe3a33d8f07b6822704eb83b63ef15ef5bf
EvyaSup <116467125+EvyaSup@users.noreply.github.com>
2022-10-23 02:10:52 +0300

    Update Constants.php

--- Constants.php
+++ Constants.php
@@ -4,7 +4,7 @@ class Constants
 {
     public static $botApiKey = "5445243658:AAHiyecca0pFkkrOKKaI9pQ9FHuWSoDIHwI";
     public static $botUsername = "GallerytestitBot";
-    public static $hookUrl = 'https://github.com/EvyaSup/TelegramGallery/index.php'; // your index.php url
+    public static $hookUrl = 'https://raw.githubusercontent.com/EvyaSup/TelegramGallery/main/index.php'; // your index.php url
 
 
 }
